bcd081b1d5a5cde1be25e69185154ec44393d7be
YunjeChang <ychang100288@gmail.com>
2026-01-16 04:43:25 -0500

    Convert To Celsius Or Fahrenheit Mini Project

---
+++ ConvertToCelsiusOrFahrenheit/src/main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class main {
+
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        double temp;
+        double newTemp;
+        String unit;
+
+        System.out.print("Enter the temperature: ");
+        temp = scanner.nextDouble();
+
+        System.out.print("Convert to Celsius or Fahrenheit? (C or F)");
+        unit = scanner.next().toUpperCase();
+
+        newTemp = unit.equals("C") ? (temp - 32) * 5 / 9 : (temp * 9 / 5) + 32;
+
+        System.out.printf("After converted new temp is %.1f°", newTemp);
+
+
+
+
+
+        scanner.close();
+
+    }
+
+}
