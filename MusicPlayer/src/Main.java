07d270666456dd45815da3afdfab9ab218dd7b86
YunjeChang <ychang100288@gmail.com>
2026-02-02 17:12:43 -0500

    Music Player

---
+++ MusicPlayer/src/Main.java
@@ -0,0 +1,72 @@
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        String filePath = "src\\On The Flip - The Grey Room _ Density & Time.wav";
+        File file = new File(filePath);
+
+        try(Scanner sc = new Scanner(System.in);
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(file)){
+
+
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioStream);
+
+
+            String response = "";
+
+            while(!response.equals("Q")){
+
+                System.out.println("P = play");
+                System.out.println("S = STOP");
+                System.out.println("R = RESET");
+                System.out.println("Q = QUIT");
+                System.out.print("Enter your choice: ");
+
+                response = sc.next().toUpperCase();
+
+                switch(response){
+                    case "P" -> clip.start();
+                    case "S" -> clip.stop();
+                    case "R" -> clip.setMicrosecondPosition(0);
+                    case "Q" -> clip.close();
+                    default -> System.out.println("Invalided choice");
+                }
+
+            }
+
+
+        }
+        catch(FileNotFoundException e){
+            System.out.println(e);
+            System.out.println("Could not locate file");
+        }
+        catch(UnsupportedAudioFileException e){
+            System.out.println(e);
+            System.out.println("Audio file is not supported");
+        }
+        catch(LineUnavailableException e){
+            System.out.println(e);
+            System.out.println("Unable to access audio resource");
+        }
+        catch(IOException e){
+            System.out.println(e);
+            System.out.println("Something went wrong");
+        }
+        finally {
+            System.out.println("Bye!");
+
+        }
+
+
+
+
+    }
+
+}
